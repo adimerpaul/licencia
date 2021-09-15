faadd0ac5506b0836678f0181fca2a1175a7f7b4
adimerpaul <adimer101@gmail.com>
2021-09-15 15:58:02 -0400

    build

--- licenciafront/quasar.conf.js
+++ licenciafront/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
       env: {
         API: ctx.dev
           ? 'http://localhost:8000/api'
-          : 'http://157.245.136.92:85/api'
+          : 'http://157.245.136.92:84/api'
       }
     },
 
